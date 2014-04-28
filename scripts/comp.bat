f8725c00e2b62525462695f1bf22a37d0972642d
OnlineCop <onlinecop+github@gmail.com>
2014-04-27 17:16:45 -0700

    No longer compiling .lua files to .lob or .plua.

--- scripts/comp.bat
+++
@@ -1,4 +0,0 @@
-perl prep.pl < %1.lua > _tmpfile.$$$
-luac -o %1.lob _tmpfile.$$$
-erase _tmpfile.$$$
-
--- scripts/compall.sh
+++
@@ -1,5 +0,0 @@
-for a in `cat allsc.txt`
-do
-  echo "Processing $a"
-  perl prep.pl < $a.lua | ./luac.exe -o $a.lob -
-done  
