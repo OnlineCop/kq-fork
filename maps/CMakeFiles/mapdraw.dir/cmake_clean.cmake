bdcc94b73089759f8ae03244bf38ce7404f70d4a
OnlineCop <onlinecop+github@gmail.com>
2015-04-13 09:14:06 -0600

    May not want to include these files. But I gotta stash them so I can pull from the repo.

---
+++ maps/CMakeFiles/mapdraw.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mapdraw.dir/media/sf_kq-fork/src/bounds.cpp.o"
+  "CMakeFiles/mapdraw.dir/media/sf_kq-fork/src/disk.cpp.o"
+  "CMakeFiles/mapdraw.dir/media/sf_kq-fork/src/markers.cpp.o"
+  "CMakeFiles/mapdraw.dir/mapdata.cpp.o"
+  "CMakeFiles/mapdraw.dir/mapedit.cpp.o"
+  "CMakeFiles/mapdraw.dir/mapent.cpp.o"
+  "CMakeFiles/mapdraw.dir/mapfile.cpp.o"
+  "CMakeFiles/mapdraw.dir/mapshared.cpp.o"
+  "CMakeFiles/mapdraw.dir/mapstructs.cpp.o"
+  "mapdraw.pdb"
+  "mapdraw"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mapdraw.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
