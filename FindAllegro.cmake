320713667391aadb48c5d06decee02caf3fe11a4
Peter Hull <peterhull90@gmail.com>
2015-04-14 12:54:23 +0100

    Add DEBUGMODE option to CMake file. Don't specify the libs explicitly as FindXXX scripts do this already. Add aldmb to libraries. Revert FindAllegro.cmake to version from Olof Naessen's fifechan project (http://github.com/fifengine/fifechan/)

--- FindAllegro.cmake
+++ FindAllegro.cmake
@@ -1,7 +1,4 @@
 # - Try to find Allegro
-# Configs for lookup
-#  ALLEGRO_PATH_SUFFIX - suffix for include path, allegro or alllegro5
-#
 # Once done this will define
 #
 #  ALLEGRO_FOUND - system has Allegro
@@ -15,14 +12,12 @@
 #  BSD license.
 #  For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 #
-if (ALLEGRO_INCLUDE_DIRS)
+
+
+if (ALLEGRO_LIBRARIES AND ALLEGRO_INCLUDE_DIRS)
   # in cache already
   set(ALLEGRO_FOUND TRUE)
 else (ALLEGRO_LIBRARIES AND ALLEGRO_INCLUDE_DIRS)
-  if (NOT DEFINED ALLEGRO_PATH_SUFFIX)
-    set(ALLEGRO_PATH_SUFFIX allegro)
-  endif()
-
   find_path(ALLEGRO_INCLUDE_DIR
     NAMES
       allegro.h
@@ -32,26 +27,42 @@ else (ALLEGRO_LIBRARIES AND ALLEGRO_INCLUDE_DIRS)
       /opt/local/include
       /sw/include
     PATH_SUFFIXES
-      "${ALLEGRO_PATH_SUFFIX}"
+      allegro
+  )
+
+  find_library(ALLEG_LIBRARY
+    NAMES
+      alleg
+    PATHS
+      /usr/lib
+      /usr/local/lib
+      /opt/local/lib
+      /sw/lib
   )
 
   set(ALLEGRO_INCLUDE_DIRS
     ${ALLEGRO_INCLUDE_DIR}
   )
+  set(ALLEGRO_LIBRARIES
+    ${ALLEG_LIBRARY}
+)
 
-  if (ALLEGRO_INCLUDE_DIRS)
+  if (ALLEGRO_INCLUDE_DIRS AND ALLEGRO_LIBRARIES)
      set(ALLEGRO_FOUND TRUE)
-  endif (ALLEGRO_INCLUDE_DIRS)
+  endif (ALLEGRO_INCLUDE_DIRS AND ALLEGRO_LIBRARIES)
 
   if (ALLEGRO_FOUND)
+    if (NOT Allegro_FIND_QUIETLY)
+      message(STATUS "Found Allegro: ${ALLEGRO_LIBRARIES}")
+    endif (NOT Allegro_FIND_QUIETLY)
   else (ALLEGRO_FOUND)
     if (Allegro_FIND_REQUIRED)
       message(FATAL_ERROR "Could not find Allegro")
     endif (Allegro_FIND_REQUIRED)
   endif (ALLEGRO_FOUND)
 
-  # show the ALLEGRO_INCLUDE_DIRS variables only in the advanced view
-  mark_as_advanced(ALLEGRO_INCLUDE_DIRS)
+  # show the ALLEGRO_INCLUDE_DIRS and ALLEGRO_LIBRARIES variables only in the advanced view
+  mark_as_advanced(ALLEGRO_INCLUDE_DIRS ALLEGRO_LIBRARIES)
 
-endif (ALLEGRO_INCLUDE_DIRS)
+endif (ALLEGRO_LIBRARIES AND ALLEGRO_INCLUDE_DIRS)
 
