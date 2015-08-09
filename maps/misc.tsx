cf6714e3e429abe0e58786bfb255c9f385cebcff
Peter Hull <peterhull90@gmail.com>
2015-08-09 12:02:33 +0100

    Added script to convert to TMX format

---
+++ maps/misc.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="misc" tilewidth="16" tileheight="16">
+ <image source="Misc.png" width="320" height="240"/>
+</tileset>
---
+++ maps/uschrs.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="uschrs" tilewidth="16" tileheight="16">
+ <image source="uschrs.png" width="192" height="192"/>
+</tileset>
