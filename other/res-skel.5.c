8bb1512dd4251428b74961bd211e6123bfbb9463
OnlineCop <onlinecop+github@gmail.com>
2016-01-05 08:36:50 -0700

    Merged encounter rable from peterhull90. Updated same values within .csv file and slightly reformatted res-skel.#.c files (although we should be removing these soon in favor of a CSV parser).

--- other/res-skel.5.c
+++ other/res-skel.5.c
@@ -2,4 +2,14 @@
 
 
 
-s_encounter battles[NUM_BATTLES] = {
+/* Format: {Map, Zone, Enc, Etnum, Eidx, "BMusic", "BackImg"}
+ *   Map:       Where this battle occurs
+ *   Zone:      What triggers this battle
+ *   Enc:       The 1-in-enc chance there will NOT be combat
+ *   Etnum:     Select rows in the Encounter table
+ *   Eidx:      Select row out of the etnum; 99 for random
+ *   "BMusic":  Music file to play
+ *   "BackImg": Background image
+ */
+s_encounter battles[NUM_BATTLES] =
+{
