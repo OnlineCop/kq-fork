415d8b09dee6512a08028b947fd01094efcc1b17
Peter Hull <peterhull90@gmail.com>
2016-05-16 08:39:44 +0100

    Added simplistic functions to get random numbers and save/restore the state

---
+++ include/random.h
@@ -0,0 +1,29 @@
+/*
+   KQ is Copyright (C) 2002 by Josh Bolduc
+
+   This file is part of KQ... a freeware RPG.
+
+   KQ is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published
+   by the Free Software Foundation; either version 2, or (at your
+   option) any later version.
+
+   KQ is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with KQ; see the file COPYING.  If not, write to
+   the Free Software Foundation,
+       675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __RANDOM_H__
+#define __RANDOM_H__
+#include <string>
+void kq_init_random();
+int kq_rnd(int v0, int v1);
+std::string kq_get_random_state();
+void kq_set_random_state(const std::string& s);
+#endif
