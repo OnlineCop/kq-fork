19f093f21a80d782af197faa5a9556253e62e1cd
OnlineCop <onlinecop@protonmail.com>
2022-07-04 07:44:13 -0600

    compat.h: Re-adding accidental removal

--- include/compat.h
+++ include/compat.h
@@ -44,6 +44,8 @@ inline int makecol(int r, int g, int b)
     return ((r & 0xff) << 16) | ((g & 0xff) << 8) | (b & 0xff);
 }
 
+void fade_interpolate(RGB*, RGB*, RGB*, int, int, int);
+
 enum eDrawMode
 {
     DRAW_MODE_SOLID = 0,
