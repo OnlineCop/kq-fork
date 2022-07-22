5508a31329a5241999932c7b7faf2f3de0c107fc
OnlineCop <onlinecop@protonmail.com>
2022-07-22 14:51:32 -0600

    itemmenu: Move comments from .cpp to .h

--- include/itemmenu.h
+++ include/itemmenu.h
@@ -33,22 +33,51 @@ enum eItemEffectResult
     ITEM_EFFECT_SUCCESS_MULTIPLE = 2
 };
 
+/*! \brief Process the item menu.
+ *
+ * This screen displays the list of items that the character has,
+ * then waits for the player to select one.
+ */
 void camp_item_menu();
 
-/*! \brief Check if we can add item quantity to inventory
+/*! \brief Check if we can add item quantity to inventory.
  *
- * This is a handy function, which checks to see if a certain quantity of a
- * specified item can be stored in the inventory.
+ * This is a handy function, which checks to see if a certain quantity of a specified item can be stored in the
+ * inventory.
  *
- * \param   inventory_index Item index
- * \param   item_quantity Item quantity
- * \returns 0 if it was not possible
- * \returns 1 if it was possible, but that we added to an item slot that
- *            already had some of that item
- * \returns 2 if we put the item in a brand-new slot
+ * \param   item_id Item index.
+ * \param   item_quantity Item quantity.
+ * \returns 1 (we added to an item slot).
  */
 int check_inventory(size_t item_id, int item_quantity);
 
+/*! \brief Perform item effects.
+ *
+ * This is kind of clunky, but it works.
+ *
+ * \param   attack_fighter_index Index of attacker.
+ * \param   fighter_index  Index of item to use.
+ * \param   ti Index of target(s).
+ * \returns ITEM_EFFECT_INEFFECTIVE if ineffective (cannot use item);
+ *          ITEM_EFFECT_SUCCESS_SINGLE if success (1 target);
+ *          ITEM_EFFECT_SUCCESS_MULTIPLE if success (multiple targets).
+ */
 eItemEffectResult item_effects(size_t attack_fighter_index, size_t fighter_index, int ti);
+
+/*! \brief Remove item from inventory.
+ *
+ * Remove an item from inventory and re-sort the list.
+ *
+ * \param   inventory_index Index of item to remove.
+ * \param   qi Quantity of item.
+ */
 void remove_item(size_t inventory_index, int qi);
+
+/*! \brief Use up an item, if we have any.
+ *
+ * Go through the inventory; if there is one or more of an item, remove it.
+ *
+ * \param   item_id The identifier (I_* constant) of the item.
+ * \returns 1 if we had it, 0 otherwise.
+ */
 int useup_item(int item_id);
