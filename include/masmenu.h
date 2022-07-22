c2e3a77a11261255a6ecf1c05c42046b706ee6b6
OnlineCop <onlinecop@protonmail.com>
2022-07-22 14:54:05 -0600

    masmenu: Move comments from .cpp to .h, update static comments

--- include/masmenu.h
+++ include/masmenu.h
@@ -21,7 +21,22 @@
 
 #pragma once
 
+/*! \brief Character learned new spells?
+ *
+ * This is called by level_up() and checks to see if the character has learned
+ * any new spells (that s/he did not already know).
+ *
+ * \param   who Character's index in party[] and s_spell::clvl[] arrays.
+ * \returns 0 if no spell learned, else number of spells learned.
+ */
 int learn_new_spells(int who);
+
+/*! \brief Spell menu.
+ *
+ * Display the magic spell menu and allow the player to arrange or cast spells.
+ *
+ * \param   c Index of caster in pidx[] array.
+ */
 void camp_spell_menu(int c);
 
 extern int close_menu;
