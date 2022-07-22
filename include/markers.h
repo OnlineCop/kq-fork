2c3ef0b8da27f0649179ccca731534f291d2953e
OnlineCop <onlinecop@protonmail.com>
2022-07-22 14:53:31 -0600

    markers: Update comments

--- include/markers.h
+++ include/markers.h
@@ -27,19 +27,16 @@
 #include <vector>
 
 /*! \file
- * \brief Structures common to mapedit and the game (s_map and s_entity)
- * \author PH
- * \date 20030805
+ * \brief Structures common to mapedit and the game (s_map and s_entity).
  */
 
-/*! \brief Position marker
+/*! \brief Position marker.
  *
  * A marker is a named position on the map.
+ *
  * These are set up in the mapeditor and accessed via scripts.
- * They are to make it easier to pinpoint locations in the maps,
- * for example, the locations that doors lead to.
- * \author PH
- * \date 20050126
+ *
+ * They are to make it easier to pinpoint locations in the maps, for example, the locations that doors lead to.
  */
 struct KMarker
 {
@@ -56,26 +53,46 @@ struct KMarker
 class KMarkers
 {
   public:
-    // Add a new marker to the map. Returns true on success, or false on failure.
+    /*! \brief Add a new marker to the map.
+     *
+     * \param   marker Marker to add to the map.
+     * \returns True on success, else false on failure.
+     */
     bool Add(KMarker&& marker);
 
-    // Remove the named marker from the map. Returns true if the marker was
-    // removed, or false if the marker was not found.
+    /*! \brief Remove the named marker from the map.
+     *
+     * \param   name Name of the marker to remove.
+     * \returns True if the marker was removed, else false if the marker was not found.
+     */
     bool Remove(const std::string& name);
 
-    // Return a pointer to the marker at the given @param index. If index is
-    // invalid, returns null.
+    /*! \brief Return a pointer to the marker at the given index.
+     *
+     * \param   index Index within s_markers[] array to get.
+     * \returns Pointer to the marker, or NULL if index is invalid.
+     */
     const KMarker* GetMarker(size_t index);
 
-    // Return a pointer to the marker that has the given @param name. If no
-    // markers by that name are found, returns null.
+    /*! \brief Return a pointer to the marker that has the given name.
+     *
+     * \param   name Name of the marker to search for.
+     * \returns Pointer to the marker, or NULL if name was not found.
+     */
     const KMarker* GetMarker(const std::string& name);
 
-    // Return a pointer to the marker whose @param x and @param y coordinates
-    // match. If no marker is at those coordinates, returns null.
+    /*! \brief Return a pointer to the marker whose x and y coordinates match.
+     *
+     * \param   x X coordinate within map to find a marker at.
+     * \param   y U coordinate within map to find a marker at.
+     * \returns Pointer to the marker, or NULL if no marker is at those coordinates.
+     */
     const KMarker* GetMarker(int32_t x, int32_t y);
 
-    // Return the number of markers in the array.
+    /*! \brief Return the number of markers on the map.
+     *
+     * \returns Total markers on the map.
+     */
     inline size_t Size() const
     {
         return m_markers.size();
