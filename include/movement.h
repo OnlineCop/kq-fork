3996777d8c7fbfdf165ac244a7204ad77c64ba08
OnlineCop <onlinecop@protonmail.com>
2022-07-22 14:54:52 -0600

    movement: Move comments from .cpp to .h, update static comments

--- include/movement.h
+++ include/movement.h
@@ -24,5 +24,22 @@
 #include <cstdint>
 #include <cstdlib>
 
+/*! \brief Path search implementation for KQ.
+ *
+ * Call this function to calculate the shortest path between a given NPC and a target point.
+ *
+ * \param[in] entity_id The ID of the entity moving around.
+ * \param[in] source_x The x coordinate of the source point.
+ * \param[in] source_y The y coordinate of the source point.
+ * \param[in] target_x The x coordinate of the target point.
+ * \param[in] target_y The y coordinate of the target point.
+ * \param[out] buffer A buffer where the result will be stored.
+ * \param[in] size The size of the result buffer.
+ * \returns Whether it could or not find the path.
+ *          0 Success,
+ *          1 No path found,
+ *          2 Path found but result buffer too small to hold the answer.
+ *          3 Misc error.
+ */
 int find_path(size_t entity_id, uint32_t source_x, uint32_t source_y, uint32_t target_x, uint32_t target_y,
               char* buffer, uint32_t size);
