db5f865cbc38de83f7ba2184535d7a05cf8ebf34
OnlineCop <onlinecop@protonmail.com>
2023-12-08 07:45:58 -0700

    Remove a redundancy in intrface.cpp

--- src/intrface.cpp
+++ src/intrface.cpp
@@ -3175,7 +3175,7 @@ static int KQ_set_noe(lua_State* L)
 {
     uint32_t a = (uint32_t)lua_tonumber(L, 1);
 
-    if (a <= MAX_ENTITIES_PER_MAP + PSIZE)
+    if (a <= MAX_ENTITIES)
     {
         EntityManager.number_of_entities = a;
     }
