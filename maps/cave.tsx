ee515042e8fb9dd0609c902defad2c159519ff90
OnlineCop <onlinecop@protonmail.com>
2023-12-13 07:34:09 -0700

    cave.tsx: Add flashing animation for green sparkle tiles

--- maps/cave.tsx
+++ maps/cave.tsx
@@ -1,6 +1,54 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="cave" tilewidth="16" tileheight="16" tilecount="500" columns="20">
  <image source="incave.png" width="320" height="400"/>
+ <tile id="30">
+  <animation>
+   <frame tileid="30" duration="2000"/>
+   <frame tileid="31" duration="100"/>
+   <frame tileid="32" duration="100"/>
+   <frame tileid="33" duration="100"/>
+   <frame tileid="34" duration="100"/>
+   <frame tileid="35" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="176">
+  <animation>
+   <frame tileid="176" duration="250"/>
+   <frame tileid="177" duration="250"/>
+   <frame tileid="178" duration="250"/>
+   <frame tileid="179" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="323">
+  <animation>
+   <frame tileid="323" duration="100"/>
+   <frame tileid="324" duration="100"/>
+   <frame tileid="325" duration="200"/>
+   <frame tileid="326" duration="500"/>
+   <frame tileid="327" duration="200"/>
+   <frame tileid="328" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="360">
+  <animation>
+   <frame tileid="360" duration="250"/>
+   <frame tileid="361" duration="250"/>
+   <frame tileid="362" duration="250"/>
+   <frame tileid="363" duration="500"/>
+   <frame tileid="364" duration="250"/>
+   <frame tileid="365" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="380">
+  <animation>
+   <frame tileid="380" duration="250"/>
+   <frame tileid="381" duration="250"/>
+   <frame tileid="382" duration="250"/>
+   <frame tileid="383" duration="500"/>
+   <frame tileid="384" duration="250"/>
+   <frame tileid="385" duration="250"/>
+  </animation>
+ </tile>
  <tile id="474">
   <animation>
    <frame tileid="474" duration="1000"/>
